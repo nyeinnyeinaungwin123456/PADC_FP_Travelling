1acca849657579f263cd86221022f27325afaf7f
Nyein\Nyein Nyein <nyeinnyeinaungwin123456@gmail.com>
2016-09-06 00:13:24 +0630

    -left-menu-setup finish (color is optional)

--- PADC_FP_Travelling/app/src/main/java/com/padc/travelling/activities/HomeActivity.java
+++ PADC_FP_Travelling/app/src/main/java/com/padc/travelling/activities/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.padc.travelling.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,7 +16,7 @@ import android.view.MenuItem;
 
 import com.padc.travelling.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+               R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,5 +64,37 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menu_attractionplaces){
+
+        }
+        else if(id == R.id.menu_tourpackages){
+
+        }
+        else if(id == R.id.menu_highway){
+
+        }
+        else if(id == R.id.menu_hotels){
+
+        }
+        else if(id == R.id.menu_restaurants){
+
+        }
+        else if(id == R.id.menu_feedback){
+
+        }
+        else if(id == R.id.menu_aboutus){
+
+        }
+
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
