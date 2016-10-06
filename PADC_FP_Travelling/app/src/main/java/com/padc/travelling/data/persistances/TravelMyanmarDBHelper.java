68f48b21a7de47676c57ccec9aa9ecf15faa032f
Nyein\Nyein Nyein <nyeinnyeinaungwin123456@gmail.com>
2016-10-06 14:07:33 +0630

    - DB Version

--- PADC_FP_Travelling/app/src/main/java/com/padc/travelling/data/persistances/TravelMyanmarDBHelper.java
+++ PADC_FP_Travelling/app/src/main/java/com/padc/travelling/data/persistances/TravelMyanmarDBHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class TravelMyanmarDBHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 35;
+    private static final int DATABASE_VERSION = 59;
     public static final String DATABASE_NAME = "travel.db";
 
 
